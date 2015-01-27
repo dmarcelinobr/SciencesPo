2df3169b2716f19c7e2494ba0d8daf0860f6e6b4
Daniel Marcelino <dmarcelino@live.com>
2015-01-27 11:47:37 -0200

    bug fixed

--- R/stats.R
+++ R/stats.R
@@ -568,24 +568,6 @@ NULL
 
 
 
-#' @encoding UTF-8
-#' @title Compute z-scores
-#' 
-#' @description Compute z-scores
-#' @param x a numeric vector
-#' @param na.rm a logical indicating whether missing values should be removed
-#' @export
-#' @examples
-#' iris %>% 
-#'   group_by(Species) %>% 
-#'   mutate(zSepal.Length = zscore(Sepal.Length)) %>% 
-#'   head()
-zscore <- function( x, na.rm=getOption("na.rm", FALSE) ) {
-  ( x - mean(x, na.rm=na.rm)) / sd(x, na.rm=na.rm)
-}
-NULL
-
-
 
 
 #' @encoding UTF-8
