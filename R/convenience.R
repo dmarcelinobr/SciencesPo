44ac2a86d97d7d4450dae28de4fc2462eee3a2a2
Daniel Marcelino <dmarcelino@live.com>
2015-02-08 12:03:13 -0200

    added some convinience fucntions

--- R/convenience.R
+++ R/convenience.R
@@ -449,11 +449,11 @@ NULL
 #' @description Return a factor variable using breaks and labels. The function will calculate the
 #' age based upon the \code{to} if given, otherwise the \code{age.var} will be used.
 #'
-#' @param from The date of origin, typically birthdate 
-#' @param to The up to date to compute the age
-#' @param breaks The numeric break guide for grouping age
-#' @param labels The labels for the age groups, can also be set to \code{labels=NULL}
-#' @param age If an age var is present
+#' @param x if a var containing the age already exists.
+#' @param from The date of origin, typically birthdate. 
+#' @param to The up to date to compute the age.
+#' @param breaks The numeric break guide for grouping age.
+#' @param labels The labels for the age groups, can also be set to \code{labels=NULL}.
 #' 
 #' @author Daniel Marcelino <dmarcelino@@live.com>
 #'
@@ -476,8 +476,7 @@ NULL
 #'
 #' @export
 #'
-ageGroups <- function (from, to, breaks, labels,
-		x = NULL) {
+ageGroups <- function (x = NULL, from, to, breaks, labels) {
     if (is.null(x)) {
         age = elapsed(from, to)
     }
@@ -848,6 +847,82 @@ NULL
 
 
 
+
+
+#' @title Filling in missing values
+#'
+#' \code{FillIn} uses values of a variable from one data set to fill in missing values in another.
+#' 
+#' @param D1 the data frame with the variable you would like to fill in.
+#' @param D2 the data frame with the variable you would like to use to fill in \code{D1}.
+#' @param Var1 a character string of the name of the variable in \code{D1} you want to fill in.
+#' @param Var2 an optional character string of variable name in \code{D2} that you would like to use to fill in.
+#' @param KeyVar a character vector of variable names that are shared by \code{D1} and \code{D2} that can be used to join the data frames.
+#' 
+#' @examples 
+#' # Create data set with missing values
+#' naDF <- data.frame(a = sample(c(1,2), 100, rep=TRUE), 
+#'                    b = sample(c(3,4), 100, rep=TRUE), 
+#'                    fNA = sample(c(100, 200, 300, 400, NA), 100, rep=TRUE))
+#'
+#' # Created full data set
+#' fillDF <- data.frame(a = c(1,2,1,2), 
+#'                      b = c(3,3,4,4),
+#'                      fFull = c(100, 200, 300, 400))
+#'
+#' # Fill in missing f's from naDF with values from fillDF
+#' FilledInData <- fillNA(naDF, fillDF, Var1 = "fNA", Var2 = "fFull", KeyVar = c("a", "b"))
+#' @import data.table
+#' @export
+fillNA  <- function(D1, D2, Var1, Var2 = NULL, KeyVar)
+{
+  # Give Var2 the same name as var1 if Var2 is NULL
+  if (is.null(Var2)){
+    Var2 <- Var1
+  } else {
+    Var2 <- Var2
+  }
+  
+  # Give var a generic name
+  names(D1)[match(Var1, names(D1))] <- "VarGen"
+  names(D2)[match(Var2, names(D2))] <- "VarGen.1"
+  
+  # Convert data frames to data.table type objects
+  D1Temp <- data.table(D1, key = KeyVar)
+  D2Temp <- data.table(D2, key = KeyVar)
+  
+  # Merge data.tables
+  OutDT <- D2Temp[D1Temp]
+  
+  # Tell the user how many values will be filled in
+  SubNA <- OutDT[, list(VarGen, VarGen.1)]
+  SubNA <- subset(SubNA, is.na(VarGen) & !is.na(VarGen.1))
+  print(paste(nrow(SubNA), "NAs were replaced."))
+  
+  # Fill in missing values from D1 with values from D2
+  OutDT <- OutDT[is.na(VarGen), VarGen := VarGen.1]
+  
+  # Convert back to data frame
+  OutDF <- data.frame(OutDT)
+  
+  # Tell the user what the correlation coefficient is between the variables
+  SubNoNA <- subset(OutDF, !is.na(VarGen) & !is.na(VarGen.1))
+  HowMany <- nrow(SubNoNA)
+  CORR <- cor(SubNoNA$VarGen, SubNoNA$VarGen.1, use = "complete.obs")
+  print(paste("The correlation between", Var1, "and", Var2, "is", round(CORR, digits = 3), "based on", HowMany, "shared observations." ))
+  
+  # Remove uncombined variable and return main variable's name
+  names(OutDF)[match("VarGen", names(OutDF))] <- Var1
+  Keepers <- setdiff(names(OutDF), "VarGen.1")
+  OutDF <- OutDF[, Keepers]
+  OutDF
+}
+NULL
+
+
+
+
+
 #' @title Fill NA by Previous Value
 #' @description Fill NA by Previous Value
 #' @param column the column that constains NAs
@@ -880,7 +955,7 @@ NULL
 
 
 #' @encoding UTF-8
-#' @title Rectangularize a dataframe Fill in missing records
+#' @title Rectangularize a data.frame by filling missing records
 #' 
 #' @description This function produces a complete rectangularization by adding observations with missing data so that all combinations (interactions) of the specified variables exist.
 #' 
@@ -893,8 +968,6 @@ NULL
 #' @examples
 #' data <- data.frame(sex=c("female","male","male"), 
 #' race=c("black","black","white"), y=c(.5,.4,.1), x=c(32,40,53))
-#' 
-#' data
 #' 
 #' fillin(data, by=c(sex,race))
 #' 
@@ -1050,3 +1123,21 @@ function (x)
   ans <- pt4[as.integer(cut(x, c(0, pt2$pt1), labels = 1:length(pt3)))]
   return(ans)
 }
+NULL
+
+
+
+#' Simple function to transform a dependent variable that in [0,1] rather than 
+#' (0, 1) to beta regression. Suggested by Smithson & Verkuilen (2006).
+#' 
+#' @param y vector of the dependent variable that is in [0, 1].
+
+svTransform <- function(y)
+{
+  n <- length(y)
+  transformed <- (y * (n-1) + 0.5)/n
+  return(transformed)
+}
+NULL
+
+
