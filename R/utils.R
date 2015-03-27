2d529dd1d6ae133f19b3607d319596e26fd0eb86
Daniel Marcelino <dmarcelino@live.com>
2015-03-27 13:32:13 -0300

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -95,3 +95,24 @@ otherNames <- function(data, check) {
   setdiff(names(data), colNames(data, check))
 }
 NULL
+
+#' @encoding UTF-8
+#' @title nobs method
+#' @param object the input object.
+#' @param \dots Some extra arguments.
+#' @note nobs generic is defined only in R 2.13.0: define here for older R
+if (getRversion() < "2.13.0")
+nobs <- function(object, ...) UseMethod("nobs")
+#' add some nobs() methods
+`nobs.numeric` <- function(object, ...) {
+length(object)
+}
+`nobs.integer` <- function(object, ...) {
+nobs.numeric(object, ...)
+}
+`nobs.matrix` <- function(object, ...) {
+NROW(object)
+}
+`nobs.data.frame` <- function(object, ...) {
+NROW(object)
+}
