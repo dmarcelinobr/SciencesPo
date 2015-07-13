8a1f4a70af2542dec223263f1d0f448c2ee1f9c8
Daniel <dmarcelino@live.com>
2015-07-13 12:58:21 -0300

    releasing version

--- R/dHondt.R
+++ R/dHondt.R
@@ -1,4 +1,4 @@
-#' @title Method D'Hont
+#' @title Method D'Hondt
 #' @description The function calculate the seats in parliament given the total number of seats and the votes for each party.
 #' @param parties A vector containig the identification of parties or candidates accordingly to the election outcome.
 #' @param votes A vector containing the total number of formal votes received by the parties/candidates.
@@ -6,12 +6,12 @@
 #' @export
 #' @examples
 #' votes <- sample(1:10000, 5)
-#' dHont(letters[1:5], votes, 10 )
+#' dHondt(letters[1:5], votes, 10 )
 #'
 #' votes <- c(42201,38635,247736,170627,48236,117151,61379,35889,92321)
 #' dHont(c("A","B","C","D","F","G","H","I","J"), votes, 26)
 #'
-dHont <- function(parties, votes, seats){
+dHondt <- function(parties, votes, seats){
   tmp <- data.frame(
     parties = rep( parties, each = seats ),
     scores = as.vector(sapply( votes, function(x) x /
