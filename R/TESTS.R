92aad658699012c5ff0bfbde421fe706c6f1a0c0
Daniel <dmarcelino@live.com>
2016-01-09 02:14:12 -0200

    :lollipop:

--- R/TESTS.R
+++ R/TESTS.R
@@ -1191,3 +1191,23 @@ NULL
 
 
 
+
+#' Returns significance level.
+#'
+#' Returns the significance level as stars, or NA if a non-numeric value is
+#' passed in.
+#'
+#' @param x p-value.
+#' @export
+star <- function(x) {
+  if(is.numeric(x)) {
+    return(cut(x,
+               breaks=c(-Inf,.001,.01,.05,.1,Inf),
+               labels=c('***','**','*','.',''))
+    )
+  } else {
+    return(NA)
+  }
+}
+NULL
+
